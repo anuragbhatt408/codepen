76c18dfddfc586c80975e638c73c7eb80fa8c48b
anuragbhatt408 <53056656+anuragbhatt408@users.noreply.github.com>
2021-07-28 09:32:47 +0530

    Updates

---
+++ precache-manifest.7602cb5c687fa954e8f94c7fb2457c59.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "447e86d1169c2acc8e61fd7a211bea95",
+    "url": "/codepen/index.html"
+  },
+  {
+    "revision": "70c3ff1a8dd6beff5ca9",
+    "url": "/codepen/static/css/2.c46397a6.chunk.css"
+  },
+  {
+    "revision": "d11d2bd7701983799949",
+    "url": "/codepen/static/css/main.7d2118c4.chunk.css"
+  },
+  {
+    "revision": "70c3ff1a8dd6beff5ca9",
+    "url": "/codepen/static/js/2.4254f81d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/codepen/static/js/2.4254f81d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d11d2bd7701983799949",
+    "url": "/codepen/static/js/main.1de2429d.chunk.js"
+  },
+  {
+    "revision": "f9ee33409a7c42d90d46",
+    "url": "/codepen/static/js/runtime-main.3a53c1ee.js"
+  }
+]);
